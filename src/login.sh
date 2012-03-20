fa7f593fd8702b209bd1f0bf8ad0fedcff7a5181
Doug McGeehan <koulevprime@gmail.com>
2012-03-20 09:39:15 -0500

    Added the user directory creation portion of the script, however it is currently commented out

--- src/login.sh
+++ src/login.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
+HOME_DIRECTORY="/home/"
+USER_HOME=""
+USER_SHELL="/bin/bash"
+
 read -p "Enter your username (e.g. djmvfb): " user
 read -sp "Enter your desired password: " password
 
+USER_HOME=${HOME_DIRECTORY}${user}
+
 # Add this user using the adduser command
+## UNCOMMENT THE LINE BELOW WHEN READY
+# echo ${user}:${password}::::${USER_HOME}:${USER_SHELL} | newusers
+echo "Now calling $ newusers ${user}:${password}::::${USER_HOME}:${USER_SHELL}" 
 
 echo "You may now log in using the username and password you provided."
 echo "You will be booted off so that you can use your own user account."
