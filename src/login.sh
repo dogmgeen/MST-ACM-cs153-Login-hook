9756da481f5b894501f44d607764fec429beca08
Doug McGeehan <djmvfb@mst.edu>
2012-03-20 13:23:35 -0500

    Put quotes around user input to protect against injection

--- src/login.sh
+++ src/login.sh
@@ -8,12 +8,12 @@ GROUP_NAME="cs153sp2012"
 read -p "Enter your username (e.g. djmvfb): " user
 read -sp "Enter your desired password: " password
 
-USER_HOME=${HOME_DIRECTORY}${user}
+USER_HOME="${HOME_DIRECTORY}${user}"
 
 # Add this user using the adduser command
 ## UNCOMMENT THE LINE BELOW WHEN READY
-# echo ${user}:${password}::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
-echo "Now calling $ newusers ${user}:${password}::${GROUP_NAME}::${USER_HOME}:${USER_SHELL}" 
+# echo \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
+echo "Now calling $ newusers \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL}" 
 
 echo "You may now log in using the username and password you provided."
 echo "You will be booted off so that you can use your own user account."
