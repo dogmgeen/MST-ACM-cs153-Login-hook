4299ae898680019ba3b0ef113289a5dc616c8f3f
Doug McGeehan <koulevprime@gmail.com>
2012-03-20 17:59:27 -0500

    Fixed a bug where the echos were echoeing too much

--- src/login.sh
+++ src/login.sh
@@ -5,20 +5,17 @@ USER_HOME=""
 USER_SHELL="/bin/bash"
 GROUP_NAME="cs153sp2012"
 
-read -p "Enter your username (e.g. djmvfb): " user
-read -sp "Enter your desired password: " password
+read -p "Enter your username (e.g. djmvfb): " user && \
+read -sp "Enter your desired password: " password && \
 
-USER_HOME="${HOME_DIRECTORY}${user}"
+USER_HOME="${HOME_DIRECTORY}${user}" && \
 
 # Add this user using the adduser command
 ## UNCOMMENT THE LINE BELOW WHEN READY
-#echo \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
-echo "Creating user home directory:"
-echo "\"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers"
+echo \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers && \
 
-echo "You may now log in using the username (\"${user}\") and password you provided."
+echo "You may now log in using the username (\"${user}\") and password you provided." && \
 echo "You will be booted off so that you can use your own user account."
 
 # Immediately log out
-#exit
 
