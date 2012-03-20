9e885f4b340d4982e23aa7b3f82a57251afa4c48
Doug McGeehan <koulevprime@gmail.com>
2012-03-20 09:21:20 -0500

    login.sh script now grabs user input and stores it. LinuxMercedes originally wrote this code up in a csshell file on the mst-acm box

---
+++ src/login.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+read -p "Enter your username (e.g. djmvfb): " user
+read -sp "Enter your desired password: " password
+
+# Add this user using the adduser command
+
+echo "You may now log in using the username and password you provided."
+echo "You will be booted off so that you can use your own user account."
+
+# Immediately log out
+#exit
+
