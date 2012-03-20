db5a01973601bff6e9ac6b262c3f0d200d73373c
Doug McGeehan <koulevprime@gmail.com>
2012-03-20 17:14:09 -0500

    Fixing up the code for deployment

--- src/login.sh
+++ src/login.sh
@@ -12,10 +12,11 @@ USER_HOME="${HOME_DIRECTORY}${user}"
 
 # Add this user using the adduser command
 ## UNCOMMENT THE LINE BELOW WHEN READY
-# echo \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
-echo "Now calling $ newusers \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL}" 
+#echo \"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
+echo "Creating user home directory:"
+echo "\"${user}\":\"${password}\"::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers"
 
-echo "You may now log in using the username and password you provided."
+echo "You may now log in using the username (\"${user}\") and password you provided."
 echo "You will be booted off so that you can use your own user account."
 
 # Immediately log out
