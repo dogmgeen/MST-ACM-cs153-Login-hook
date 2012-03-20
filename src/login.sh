b2aae7e5b62bf8472eb2d16f89c2c653f72b7168
Doug McGeehan <koulevprime@gmail.com>
2012-03-20 09:49:21 -0500

    All users will now be added to the usergroup cs153sp2012

--- src/login.sh
+++ src/login.sh
@@ -3,6 +3,7 @@
 HOME_DIRECTORY="/home/"
 USER_HOME=""
 USER_SHELL="/bin/bash"
+GROUP_NAME="cs153sp2012"
 
 read -p "Enter your username (e.g. djmvfb): " user
 read -sp "Enter your desired password: " password
@@ -11,8 +12,8 @@ USER_HOME=${HOME_DIRECTORY}${user}
 
 # Add this user using the adduser command
 ## UNCOMMENT THE LINE BELOW WHEN READY
-# echo ${user}:${password}::::${USER_HOME}:${USER_SHELL} | newusers
-echo "Now calling $ newusers ${user}:${password}::::${USER_HOME}:${USER_SHELL}" 
+# echo ${user}:${password}::${GROUP_NAME}::${USER_HOME}:${USER_SHELL} | newusers
+echo "Now calling $ newusers ${user}:${password}::${GROUP_NAME}::${USER_HOME}:${USER_SHELL}" 
 
 echo "You may now log in using the username and password you provided."
 echo "You will be booted off so that you can use your own user account."
